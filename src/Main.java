a963520b395666b7b7472fe9c87b8a70ea74077c
Hanser Wei <2628273921@qq.com>
2024-10-25 08:52:16 +0800

    试用一下fitten

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello and welcome!\n");
-
+// 九九乘法表
         for (int i = 1; i <= 9; i++) {
             for (int j = 1; j <= i; j++) {
-                if (i == j) {
-                    System.out.print(i+"*"+j+"="+i*j+"\n");
-                }
-                else {
-                    System.out.print(i+"*"+j+"="+i*j+"\t");
-                }
+                System.out.print(i + "x" + j + "=" + i * j + "\t");
             }
+            System.out.println();
         }
     }
 }
